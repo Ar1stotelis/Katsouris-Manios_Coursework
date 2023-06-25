f835cc6256bcb8d20c8e22e593bf611bd4b68836
Aristotelis Manios <A.Manios@acg.edu>
2023-06-25 15:59:23 +0300

    Added footer and header to the index page and fixed buttons to work. Also changed the general list to a movie specific list. Added the ability to put a movie title and a score from 1 to 10

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ $(document).ready(function() {
     var sortDirection = 1;
 
     // Adding to the list of items
-    function addItem(item, quantity) {
-        items.push({ item: item, quantity: quantity });
+    function addItem(movie, score) {
+        items.push({ movie: movie, score: score });
         updateTable();
     }
 
-    // Function to remove from list
+    // Function to remove from the list
     function removeItem(index) {
         items.splice(index, 1);
         updateTable();
@@ -19,13 +19,13 @@ $(document).ready(function() {
 
     // Sort the items based on the current sorting column and direction
     function sortItems() {
-        if (sortColumn === 'quantity') {
+        if (sortColumn === 'score') {
             items.sort(function(a, b) {
-                return sortDirection * (a.quantity - b.quantity);
+                return sortDirection * (a.score - b.score);
             });
-        } else if (sortColumn === 'item') {
+        } else if (sortColumn === 'movie') {
             items.sort(function(a, b) {
-                return sortDirection * a.item.localeCompare(b.item);
+                return sortDirection * a.movie.localeCompare(b.movie);
             });
         }
     }
@@ -44,8 +44,8 @@ $(document).ready(function() {
         for (var i = 0; i < items.length; i++) {
             var item = items[i];
             var row = $('<tr>');
-            row.append($('<td>').text(item.item));
-            row.append($('<td>').text(item.quantity));
+            row.append($('<td>').text(item.movie));
+            row.append($('<td>').text(item.score));
             row.append($('<td>').html('<button class="btn btn-danger delete-btn" data-index="' + i + '">Delete</button>'));
             tableBody.append(row);
         }
@@ -54,11 +54,11 @@ $(document).ready(function() {
     // Data submission
     $("#addItemForm").submit(function(event) {
         event.preventDefault();
-        var item = $("#item").val();
-        var quantity = $("#quantity").val();
-        addItem(item, quantity);
-        $("#item").val("");
-        $("#quantity").val("");
+        var movie = $("#movie").val();
+        var score = parseInt($("#score").val());
+        addItem(movie, score);
+        $("#movie").val("");
+        $("#score").val("");
     });
 
     // Delete button click
